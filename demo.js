c62e744712f225eac6c66d26f0124b3f26b5fdae
Swetha V <sv6@corp.logitech.com>
2026-07-29 21:16:58 +0530

    Add tracker dashboard and demo files

---
+++ demo.js
@@ -0,0 +1,63 @@
+var a=10
+var b=30
+console.log(a+b)
+//function without return type
+function abc()
+{
+    console.log("Hello World")
+}
+abc()
+//function without return type
+function iphone()
+{
+    console.log("Iphone 14 pro max")
+}
+function Redmi()
+{
+        console.log("Redmi Note 12")
+}
+
+function Oppo()
+{
+      console.log("Oppo Reno 8")
+}
+iphone()
+Redmi()
+Oppo()
+
+var a=20
+var b=30
+function add()
+{
+    console.log(a+b)
+}
+add()
+
+
+var factor="Kamal"
+var fplayer="Dhoni"
+var fmovie="Anbe Sivam"
+function favorite()
+{
+    console.log("Favorite Actor:",factor)
+    console.log("Favorite Player:",fplayer)
+    console.log("Favorite Movie:"+fmovie)
+}
+favorite()
+ //return type function
+function area(l,b)
+{
+    return l*b
+}
+console.log("Area of rectangle:", area(10,20))
+
+
+
+function sum(a,b)
+{
+    return a+b
+}
+console.log("Sum:", sum(10,20))
+
+
+
