d8b4156d3c8f969c520c9352b42eefe305e22461
SwethaV005 <sv6@logitech.com>
2026-09-12 14:27:03 +0530

    Add Van Page (react-router)

--- React-router/src/index.jsx
+++ React-router/src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './pages/Home'
 import About from './pages/About'
+import Van from './pages/Vans'
+import './pages/server'
 
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
@@ -14,11 +16,13 @@ function App() {
   <nav>
     
     <Link to="/About">About</Link>
+    <Link to="/Vans">Van</Link>
     </nav>
     </header>
-    <Routes>
+    <Routes> 
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />}/>
+      <Route path="/vans" element={<Van />}/>
     </Routes>
   </BrowserRouter> 
   )
