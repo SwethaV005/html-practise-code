da2d810337d91f9e3cf8345724e4a893544ddef1
SwethaV005 <sv6@logitech.com>
2026-09-15 11:30:27 +0530

    Add Layout route & Nested Route

--- React-router/src/index.jsx
+++ React-router/src/index.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './pages/Home'
 import About from './pages/About'
-import Van from './pages/Vans'
-import Vandetail from './pages/Vandetail'
+import Van from './pages/Van/Vans'
+import Vandetail from './pages/Van/Vandetail'
+import Layout from './components/Layout'
+import Dashboard from './pages/Host/Dashboard'
+import Income from './pages/Host/Income';
+import Review from './pages/Host/Review'
+import HostLayout from './components/HostLayout'
 import './pages/server'
 
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
@@ -12,19 +17,24 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 function App() {
   return(
   <BrowserRouter>
-  <header>
-     <Link to="/" className="site-logo">#VANLIFE</Link>
-  <nav>
-    
-    <Link to="/about">About</Link>
-    <Link to="/vans">Van</Link>
-    </nav>
-    </header>
+
     <Routes> 
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />}/>
-      <Route path="/vans" element={<Van />}/>
-      <Route path="/vans/:id" element={<Vandetail/>}/>
+      <Route path="/" element={<Layout/>}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />}/>
+      {/* <Route path="vans" element={<Van />}/>
+      <Route path="vans/:id" element={<Vandetail/>}/> */}
+      /*Converting the code into nested route not layout route because/vans and /vans/:id does not haVe shared UI*/
+      <Route path="vans"  >
+        <Route index element={<Van/>}/>
+        <Route path=":id" element={<Vandetail/>}/>
+      </Route>
+      <Route path="host" element={<HostLayout/>}>
+           <Route index element={<Dashboard />}/>
+          <Route path="income" element={<Income />}/>
+          <Route path="review" element={<Review />}/>
+     </Route>
+      </Route>
     </Routes>
   </BrowserRouter> 
   )
---
+++ React-router/src/pages/Host/Review.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+export default function Review(){
+    return(
+        <h1>Review</h1>
+    )
+}
